06ba1743d7face73ee73eaa8af7b05fa06fe08d7
Ross Scarborough <rossscarborough@gmail.com>
2017-09-18 14:59:51 -0400

    First commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+var itemArray = [];
+function addToArray(form) {
+  var item = $('#textBox').val();
+  if (item.length == 0) {
+    alert('Please type a value');
+  } else if (itemArray.includes(item)) {
+    alert('That value already exists');
+  } else {
+    itemArray.push(item);
+    $('#field1').text(itemArray.join(', '));
+    $('#textBox').val("");
+  }
+}
+document.querySelector('#textBox').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+      srch(this.form);
+    }
+});
+function getRandom() {
+  var randomOutput = itemArray[Math.floor(Math.random() * itemArray.length)];
+  if ($('#noRepeat').is(":checked")) {
+    if (randomOutput == $('#output').text()) {
+      getRandom();
+    } else {
+      $('#output').text(randomOutput);
+    }
+  } else {
+    $('#output').text(randomOutput);
+  }
+}
+function clearArray() {
+  itemArray = [];
+  $('#field1').text(itemArray);
+  $('#output').text('');
+}
+function testForm() {
+  if ($('#noRepeat').is(":checked")) {
+  $('#formtest').text('true');
+  } else {
+    $('#formtest').text('false');
+  }
+}
