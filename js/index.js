ca6270308b5e45f45a3f1c90ef9a7fdd738200ec
Ross Scarborough <rossscarborough@gmail.com>
2017-09-19 16:30:53 -0400

    Some extra styling and animation for results

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-var itemArray = [];
+var itemArray = ['Taehee', 'Ross', 'Luc', 'Sona', 'Houston', 'Jiyoung'];
+$('#field1').text(itemArray.join(', '));
 function addToArray(form) {
   var item = $('#textBox').val();
   if (item.length == 0) {
@@ -12,21 +13,65 @@ function addToArray(form) {
   }
 }
 document.querySelector('#textBox').addEventListener('keypress', function (e) {
-    var key = e.which || e.keyCode;
-    if (key === 13) { 
-      srch(this.form);
-    }
+  var key = e.which || e.keyCode;
+  if (key === 13) {
+    srch(this.form);
+  }
 });
 function getRandom() {
   var randomOutput = itemArray[Math.floor(Math.random() * itemArray.length)];
-  if ($('#noRepeat').is(":checked")) {
-    if (randomOutput == $('#output').text()) {
-      getRandom();
-    } else {
-      $('#output').text(randomOutput);
-    }
+  if (itemArray.length == 0) {
+    alert('Please enter a value into the array');
   } else {
-    $('#output').text(randomOutput);
+    if ($('#noRepeat').is(":checked")) {
+      if (randomOutput == $('#output').text()) {
+        getRandom();
+      } else {
+        setTimeout(function(){
+          $('.btn-big').css('display', 'none');
+          $('#output').css('font-size', '40px');
+          $('#output').text(3);
+          $('#spinner').html('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>');
+        }, 0);
+        setTimeout(function(){
+          $('#output').text(2);
+        }, 1000);
+        setTimeout(function(){
+          $('#output').text(1);
+        }, 2000);
+        setTimeout(function(){
+          $('#spinner').html('');
+          $('#output').text(randomOutput);
+          $('#output').animate({
+            fontSize: 80,
+          }, 100);
+          $('#output').effect('bounce', 'slow');
+          $('.btn-big').css('display', 'block');
+        }, 3000);
+      }
+    } else {
+      setTimeout(function(){
+        $('.btn-big').css('display', 'none');
+        $('#output').css('font-size', '40px');
+        $('#output').text(3);
+        $('#spinner').html('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>');
+      }, 0);
+      setTimeout(function(){
+        $('#output').text(2);
+      }, 1000);
+      setTimeout(function(){
+        $('#output').text(1);
+      }, 2000);
+      setTimeout(function(){
+        $('#spinner').html('');
+        $('#output').text(randomOutput);
+        $('#output').animate({
+          fontSize: 80,
+        }, 100);
+        $('#output').effect('bounce', 'slow');
+        $('.btn-big').css('display', 'block');
+      }, 3000);
+    }
   }
 }
 function clearArray() {
@@ -36,7 +81,7 @@ function clearArray() {
 }
 function testForm() {
   if ($('#noRepeat').is(":checked")) {
-  $('#formtest').text('true');
+    $('#formtest').text('true');
   } else {
     $('#formtest').text('false');
   }
